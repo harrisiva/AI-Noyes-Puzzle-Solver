79914ef20dbbce85812e975e0c7b4dbc38440e5c
Harri Sivakumar <siva6770@mylaurier.ca>
2022-10-17 14:44:15 -0400

    Generated 3 sets

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ def debug(node:Node):
     return
 
 GOAL_STATE = [0,1,2,3,4,5,6,7,8]
-def graph_search(initial_state:Node):
+def graph_search(initial_state:Node, EXPLORED_SET):
     frontier: Frontier = Frontier() # Initialize the frontier
     frontier.insert(initial_state) 
-    EXPLORED_SET = [] # Initialize the explored set to be empty
     while True:
         if frontier.is_empty(): return False
         leaf_node: Node = frontier.pop() # Choose a leaf node and remove it from the frontier
@@ -25,33 +24,24 @@ def graph_search(initial_state:Node):
 # Generate all permutations, given the goal state. Filter by disorder parameter.
 initial_states = [list(permutation) for permutation in permutations(GOAL_STATE) if (get_disorder_parameter(list(permutation))%2==0)]
 # Select the first hundered (or sort, possibly using the same insort right algo, by smallest to largest disorder parameter and then select the first 100) and convert them to nodes
-initial_state_nodes=[Node(state) for state in initial_states[1:101]] # The first one is the goal state
+set_1 = [Node(state) for state in initial_states[1:26]]
+set_2 = [Node(state) for state in initial_states[100:125]]
+set_3 = [Node(state) for state in initial_states[50000:50025]]
+
+print(len(set_1))
+print(len(set_2))
+print(len(set_3))
+
+#initial_state_nodes=[Node(state) for state in initial_states[1:101]] # The first one is the goal state
 
 
+#print(initial_state_nodes[50])
+EXPLORED_SET = []
+print(set_3[0])
+solution = graph_search(set_3[0],EXPLORED_SET)
+print(solution.pcost)
+print(len(EXPLORED_SET))
 
-def count_of(solution:Node):
-    count = 0
-    while solution!=None:
-        count += 1
-        solution=solution.parent
-    return count
-
-
-for initial_node in initial_state_nodes:
-    debug(initial_node)
-    solution = graph_search(initial_node)
-    if solution!=False: print(f'Solution Cost: {count_of(solution)}\n')
-
-
-"""
-start_time = time.time()
-solution = graph_search(Node(initial_state))
-end_time = time.time()
-if solution!=False:
-    count = 0
-    while solution!=None:
-        solution=solution.parent
-        count += 1
-    print(count)
-    print(end_time-start_time)
-"""
+# Required Data:
+# Number of steps to find the solution
+# Number of nodes expanded by A* in each case
