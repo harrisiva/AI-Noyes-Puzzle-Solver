580690fce3949739cd177950814b4f461b325e22
harrisiva <harrishiv6@gmail.com>
2022-10-16 20:05:32 -0400

    Wrote the function for the script

--- test2.py
+++ test2.py
@@ -7,9 +7,42 @@ def debug(node:Node):
     print(f'Path Cost: {node.pcost} Heuristic: {node.heuristic} Evaluative Function: {node.evaluef}')
     return
 
+frontier = Frontier()
+
 expanded = expand_state(Node(node))
 
-debug(Node(node))
-debug(expanded[0])
-debug(expanded[1])
-debug(expanded[2])
+parent = Node(node)
+frontier.insert(parent)
+debug(parent)
+print(frontier)
+
+node1 = expanded[0]
+debug(node1)
+frontier.insert(node1)
+print(frontier)
+
+node2 = expanded[1]
+debug(node2)
+frontier.insert(node2)
+print(frontier)
+
+node3 = expanded[2]
+debug(node3)
+frontier.insert(node3)
+print(frontier)
+
+
+expanded2 = expand_state(node3)
+
+node4 = expanded2[0]
+debug(node4)
+frontier.insert(node4)
+print(frontier)
+
+node5 = expanded2[1]
+debug(node5)
+frontier.insert(node5)
+print(frontier)
+
+
+expanded2 = expand_state(node3)
